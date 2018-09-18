5eade7aad94732ffe51c1dacc8d18b7c7cfeebdd
Vishnu <vishnu@kanndigital.com>
2018-09-18 14:29:11 +0530

    migrations added

--- djangoforum/settings.py
+++ djangoforum/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'articles.apps.ArticlesConfig'
+    'articles.apps.ArticlesConfig',
+    'topics.apps.TopicsConfig'
 ]
 
 MIDDLEWARE = [
