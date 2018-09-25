1baf01b8e49fd621b31009585e7b84ed982ffc21
Vishnu <vishnu@kanndigital.com>
2018-09-25 22:42:13 +0530

    templates and static folder settings modified

--- articles/urls.py
+++ articles/urls.py
@@ -3,10 +3,11 @@ from django.urls import path
 from django.conf.urls import url,include
 from articles import views
 from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'articles'
 
 urlpatterns = [
+    path('', views.index,name='index'),
     path('admin/', admin.site.urls),
-    url(r'^index/$',views.index,name='index'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
