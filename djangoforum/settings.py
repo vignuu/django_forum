e9960714d2a7198653741a33cfc66195c62836be
vignu <vignesh.tk@kanndigital.com>
2018-09-25 20:43:08 +0530

    templates moved to article

--- djangoforum/settings.py
+++ djangoforum/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'djangoforum.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -132,3 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+        os.path.join(BASE_DIR,'assests'),
+    ) 
