3a475dc006851b757462e993d8ca9ae4d16e83c5
vignu <vignesh.tk@kanndigital.com>
2018-10-01 00:15:48 +0530

    categories created with article

--- articles/views.py
+++ articles/views.py
@@ -1,13 +1,25 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.conf.urls import url
 from .import views
 from django.contrib.auth.decorators import login_required
+from articles.models import Articles, Categories
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 
 @login_required(login_url='accounts:login')
 def index(request):
-	return render(request,'index.html')
+	if request.method == 'POST':
+		form = Categories(category_title = request.POST['categories'])
+		form.save()
+		form = Articles(title = request.POST['title'], article = request.POST.get('article'), user = request.user,categorie_id = Categories.objects.latest('id') )
+		form.save()
+		return redirect('articles:index')
+	article = Articles.objects.all().order_by('updated_at')
+	paginator = Paginator(article, 25) # Show 25 contacts per page
+	page = request.GET.get('page')
+	article = paginator.get_page(page)
+	return render(request,'index.html',{'article' : article})
 
 @login_required(login_url='accounts:login')
 def about(request):
